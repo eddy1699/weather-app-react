2243f15a66715dc34b9aaf5e51448d69ce55dc05
Edgardo <edgardosamame@gmail.com>
2023-01-02 15:58:59 -0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8f58956ec1msh5876bf1b0ff2752p15a0f1jsncb112796f3b5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const  GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const  WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = '0eb71014996abd731b48675365bcd122'
